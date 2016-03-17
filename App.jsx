6783e94fc9a9988630cdb42f8503bc39e53bbdc2
Taylor Watson <TaylorWatson18@iCloud.com>
2016-03-17 15:59:13 -0400

    create App

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+				<div>
+					<h2>Weed Maps Blog</h2>
+					<form>
+						{/* ... some sort of form. */}
+					</form>
+				</div>
+			)
+	}
+}
