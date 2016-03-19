3830d33479a147ad1cec2b8357631222a30f887a
Taylor Watson <TaylorWatson18@iCloud.com>
2016-03-18 22:47:27 -0400

    add db and form

--- App.jsx
+++ App.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
+Posts = new Meteor.Collection('posts');
+
+export default class App extends React {
+
+
+	addBlogPost(event) {
+		event.preventDefault();
+		var title = this.refs.title.value.trim();
+		var description = this.refs.description.value.trim();
+		var user;
+		Posts.insert({
+			title: title,
+			description: description,
+			createdAt: new Date()
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Weed Maps Blog</h2>
-				<form>
-					{/* ... some sort of form. */}
+				<form className='blog-form' onSubmit={this.addBlogPost.bind(this)}>
+					<input
+					type='text'
+					ref='title'
+					placeholder='Title' />
+				<br />
+				<input type='textarea'
+					rows='4'
+					ref='description'
+					placeholder='description' />
+				<br />
+				<button value='submit'>Add Post</button>
 				</form>
 			</div>
 		)
